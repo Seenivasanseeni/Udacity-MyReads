842f30fb9c3c7aa009954a44dea02863fd6dada5
SeenivasanSeeni <seeni0424@gmail.com>
2018-10-30 15:02:30 +0530

    showing Thumbnail

--- src/Book.js
+++ src/Book.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react'
 
 function Book(props){
-    const book={props};
+    const {book}=props;
     console.log(book);
     return (
             <div className="book">
                 <div className="book-top">
                 <div className="book-cover" style={{ 
-                    width: 128, height: 193, backgroundImage: `url(`+book.url+`)` //or  `url(${book.url})`
+                    width: 128, height: 193, backgroundImage:  `url(${book.imageLinks.thumbnail})`
                     }}></div>
                 <div className="book-shelf-changer">
                     <select>
