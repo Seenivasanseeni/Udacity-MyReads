ccf7c8611ada16547737694fcc86d0ee535e804c
SeenivasanSeeni <seeni0424@gmail.com>
2018-10-31 17:27:23 +0530

    cleanr and working search

--- src/Book.js
+++ src/Book.js
@@ -16,10 +16,10 @@ class Book extends Component{
                     <div className="book-shelf-changer">
                         <select onChange={this.updateCategoryHanlder}>
                         <option value="move" disabled>Move to...</option>
+                        <option value="none">None</option>
                         <option value="currentlyReading">Currently Reading</option>
                         <option value="wantToRead">Want to Read</option>
                         <option value="read">Read</option>
-                        <option value="none">None</option>
                         </select>
                     </div>
                     </div>
